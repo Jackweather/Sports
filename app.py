02cf3c8443146f96b90156dfb581e749ad5545d3
Jackweather <159730073+Jackweather@users.noreply.github.com>
2026-06-03 13:14:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ def run_task1():
     def run_all_scripts():
         print('Flask is running as user:', getpass.getuser())
         scripts = [
-            ("/opt/render/project/src/mlb/Yankees.py", "/opt/render/project/src/mlb"),
+            ("/opt/render/project/src/mlb/yankees/Yankees.py", "/opt/render/project/src/mlb/yankees/"),
         ]
         for script, cwd in scripts:
             try:
